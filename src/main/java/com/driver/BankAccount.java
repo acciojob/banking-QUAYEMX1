e658cb966beac238d81b055062b5e20a353799d4
Md Quayem Ashraf <quayemjmi@gmail.com>
2024-02-26 18:07:09 +0530

    added4

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -36,26 +36,63 @@ public class BankAccount {
         this.minBalance = minBalance;
     }
 
-    public String generateAccountNumber(int digits, int sum) throws Exception{
+    public String generateAccountNumber(int digits, int sum) throws Exception {
         //Each digit of an account number can lie between 0 and 9 (both inclusive)
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
 
 
-        int num=0;
-        int num1=digits;
+        String ans = "";
 
-        while(digits>0){
-            num+=digits%10;
-            digits/=10;
+        while (digits > 0) {
+            if (sum >= 9) {
+                sum = sum - 9;
+                digits--;
+                ans += String.valueOf(9);
+            } else if (sum == 8) {
+                sum -= 8;
+                digits--;
+                ans += String.valueOf(8);
+            } else if (sum == 7) {
+                sum -= 7;
+                digits--;
+                ans += String.valueOf(7);
+            } else if (sum == 6) {
+                sum -= 6;
+                digits--;
+                ans += String.valueOf(6);
+            } else if (sum == 5) {
+                sum -= 5;
+                digits--;
+                ans += String.valueOf(5);
+            } else if (sum == 4) {
+                sum -= 4;
+                digits--;
+                ans += String.valueOf(4);
+            } else if (sum == 3) {
+                sum -= 3;
+                digits--;
+                ans += String.valueOf(3);
+            } else if (sum == 2) {
+                sum -= 2;
+                digits--;
+                ans += String.valueOf(2);
+            } else if (sum == 1) {
+                sum -= 1;
+                digits--;
+                ans += String.valueOf(1);
+            } else if (sum == 0) {
+                sum -= 0;
+                digits--;
+                ans += String.valueOf(0);
+            }
         }
 
-        if(num!=sum){
+        if (sum != 0) {
             throw new Exception("Account Number can not be generated");
         }
-       digits=num1;
-       String s=String.valueOf(digits);
-        return s;
+
+        return ans;
     }
 
     public void deposit(double amount) {
