072ab785e294fb4c114683437f17ea8def2ab9b0
Md Quayem Ashraf <quayemjmi@gmail.com>
2024-02-26 16:33:38 +0530

    banking

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -1,11 +1,20 @@
 package com.driver;
 
+import java.util.HashMap;
+
 public class CurrentAccount extends BankAccount{
     String tradeLicenseId; //consists of Uppercase English characters only
 
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
 
+        super(name,balance,5000);
+
+        if(balance<5000){
+            throw new Exception("Insufficient Balance");
+        }
+
+        this.tradeLicenseId=tradeLicenseId;
     }
 
     public void validateLicenseId() throws Exception {
@@ -14,6 +23,19 @@ public class CurrentAccount extends BankAccount{
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
 
+        HashMap<Character,Integer>mp=new HashMap<>();
+
+        int n=tradeLicenseId.length()/2;
+
+        for(Character ch:tradeLicenseId.toCharArray()){
+            mp.put(ch,mp.getOrDefault(ch,0)+1);
+        }
+
+        for(Character c:mp.keySet()){
+            if(mp.get(c)>n){
+                throw new Exception("Valid License can not be generated");
+            }
+        }
     }
 
 }
--- src/main/java/com/driver/SavingsAccount.java
+++ src/main/java/com/driver/SavingsAccount.java
@@ -7,22 +7,47 @@ public class SavingsAccount extends BankAccount{
     public SavingsAccount(String name, double balance, double maxWithdrawalLimit, double rate) {
         // minimum balance is 0 by default
 
+        super(name,balance,0);
+        this.maxWithdrawalLimit=maxWithdrawalLimit;
+        this.rate=rate;
     }
+
     public void withdraw(double amount) throws Exception {
         // Might throw the following errors:
         // 1. "Maximum Withdraw Limit Exceed" : If the amount exceeds maximum withdrawal limit
         // 2. "Insufficient Balance" : If the amount exceeds balance
+
+        if(amount>maxWithdrawalLimit){
+            throw new Exception("Maximum Withdraw Limit Exceed" );
+        }else if(amount>getBalance()){
+            throw new Exception("Insufficient Balance");
+        }
+
+        setBalance(getBalance()-amount);
 
     }
 
     public double getSimpleInterest(int years){
         // Return the final amount considering that bank gives simple interest on current amount
 
+
+        double si=getBalance()*rate*years;
+               si=si/100;
+
+         return si;
+
     }
 
     public double getCompoundInterest(int times, int years){
         // Return the final amount considering that bank gives compound interest on current amount given times per year
 
+        double ans=(double)Math.pow(1+rate/times,years);
+
+        double amount=getBalance()*ans;
+
+        double ci=amount-getBalance();
+
+        return ci;
     }
 
 }
